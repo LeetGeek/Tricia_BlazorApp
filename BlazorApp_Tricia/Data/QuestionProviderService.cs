33fe147c39128e7e2f01d258cbb03e8c12f026ac
JVH <jjvh84@gmail.com>
2022-02-16 16:38:13 -0600

    first commit

---
+++ BlazorApp_Tricia/Data/QuestionProviderService.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorApp_Tricia.Data
+{
+    public class QuestionProviderService
+    {
+        public Task<TestQuestion[]> GetTestQuestions()
+        {
+            return Task.FromResult(new TestQuestion[2]
+            {
+                new TestQuestion()
+                {
+                    Question= "What is fermentation?",
+                    Answer1= "A process where a sugar is broken down into ethanol and carbon dioxide.",
+                    Answer2 = "A process where a sugar is broken down into yeast and carbon dioxide.",
+                    Answer3 = "A process where yeast in broken down to ethanol and carbon dioxide.",
+                    Answer4 = "A process where sugar is broken down to ethanol and yeast.",
+                    CorrectAnswer = 1
+                },
+                new TestQuestion()
+                {
+                   Question= "In the fermentation process, what molecule is produced as yeast consume sugars?",
+                   Answer1="Lactic Acid",
+                   Answer2="Glucose",
+                   Answer3="ATP",
+                   Answer4="Carbon Dioxide",
+                   CorrectAnswer=3                
+                }
+
+            });
+        }
+    }
+}
---
+++ BlazorApp_Tricia/Data/TestQuestion.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp_Tricia.Data
+{
+    public class TestQuestion
+    {
+        public string Question { get; set; }
+        public string Answer1 { get; set; }
+        public string Answer2 { get; set; }
+        public string Answer3 { get; set; }
+        public string Answer4 { get; set; }
+        public string Answer5 { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+}
